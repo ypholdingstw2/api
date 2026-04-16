85bbced4f1bbe43ded0869cc8ef9fb9c9049a413
YP Holdings <ypholdingstw2@gmail.com>
2026-04-17 06:16:41 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/api/', // 这里必须和你的GitHub仓库名一致，注意前后的斜杠
+  base: '/', // 这里必须和你的GitHub仓库名一致，注意前后的斜杠
 })
