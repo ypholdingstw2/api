03833fd9208df1eb398c21d4be173c71de62a1ba
ypholdingstw2 <ypholdingstw2@gmail.com>
2026-04-16 20:58:47 +0800

    Fix base path and add CNAME to public

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -14,7 +14,8 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   // base: 'ypholdingstw2/api/',  // 這裡放你的 GitHub 倉庫名稱
-  base: '/api/',  // 這裡放你的 GitHub 倉庫名稱
+  // base: '/api/',  // 這裡放你的 GitHub 倉庫名稱
+  base: '/',  // // 关键：使用自定义域名时设为 '/'，而非 '/api/'
   server: {
     port: 5173,
     proxy: {
